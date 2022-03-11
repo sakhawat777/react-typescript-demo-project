55b4efd9a4ae81566e92dbfd6b3f902ff8bd1312
sakhawat777 <biplobbms@gmail.com>
2022-03-11 11:16:50 +0600

    Banner Added

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Banner from './components/Banner';
 
 function App() {
   return (
     <div>
      <Header/>
+     <Banner/>
     </div>
   );
 }
